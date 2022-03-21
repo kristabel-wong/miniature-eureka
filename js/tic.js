09be94e36e01ed9e0aa7f9a2bb931f5bdeb2a1b2
kristabel-wong <kristabel.wong@hotmail.com>
2022-03-22 10:01:06 +1100

    building arrays for combos

--- js/tic.js
+++ js/tic.js
@@ -4,7 +4,72 @@ const playerX = 'X';
 const playerO = 'O';
 let playerOTurn = false;
 
-const winningCombinations = [
+
+
+// might make empty array and paste them in. 
+
+let combosX = []; // this will push an array of ID names, which is compare to combos
+let combosO = [];
+
+let xCombo1 = [];
+let xCombo2 = [];
+
+let oCombo1 = [];
+let oCombo2 = [];
+
+$('.box').on('click', function () { // applies to all box, but will choose specific box
+    let turns = 0;
+    if ( $(this).text() === ""){
+
+        if (playerOTurn === true) {
+            $( this ).text("0").css('font-size', '800%');
+            
+            if (oCombo1.length === 3) { // time to push new array 
+                combosO.push(oCombo1);
+                oCombo2.push($(this).attr('id'));
+                console.log(oCombo2);
+                console.log(combosO);
+            } else {
+                oCombo1.push($(this).attr('id'));
+                console.log(oCombo1);
+                console.log(oCombo1.length);
+            }
+            playerOTurn = false;
+        } else {
+            $( this ).text("X").css('font-size', '800%');
+            
+            if (turns === 3) {
+                combosX.push(xCombo1);
+                xCombo2.push($(this).attr('id'));
+                console.log(xCombo2);
+                console.log(combosX);
+                turns += 1;
+                console.log(turns);
+            } else if ( turns > 3) {
+                xCombo2.push($(this).attr('id'));
+                console.log(xCombo2);
+                console.log(combosX);
+                turns += 1;
+                console.log(turns);
+            } else {
+                xCombo1.push($(this).attr('id'));
+                console.log(xCombo1);
+                turns += 1;
+                console.log(turns);
+            }
+          
+            playerOTurn = true;
+        }
+    }
+    
+    
+})
+
+
+
+});
+
+const winningCombos = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -15,24 +80,6 @@ const winningCombinations = [
     [2,4,6]
 ];
 
-const gameboard = [
-    [$('#0'),$('#1'), $('#2')],
-    [$('#3'), $('#4'), $('#5')],
-    [$('#6'), $('#7'), $('#8')]
-];
-
-$('.box').on('click', function () { // applies to all box, but will choose specific box
-    if (playerOTurn === true) {
-        $( this ).text("0").css('font-size', '800%');
-        playerOTurn = false;
-    } else {
-        $( this ).text("X").css('font-size', '800%');
-        playerOTurn = true;
-    }
-    
-})
-
-});
 
 
 
